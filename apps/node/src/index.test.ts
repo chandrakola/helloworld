d810278d9d0075f6acc121117d8569b3cc6d6d71
Chandra Kola <5986878+chandrakola@users.noreply.github.com>
2026-01-05 23:24:20 -0500

    CI/CD testing with BOM

--- apps/node/src/index.test.ts
+++ apps/node/src/index.test.ts
@@ -3,13 +3,13 @@ import express from 'express';
 
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello World from Node with TypeScript!!!');
+    res.send('Hello World from Node with TypeScript!');
 });
 
 describe('GET /', () => {
     it('responds with Hello World', async () => {
         const response = await request(app).get('/');
         expect(response.status).toBe(200);
-        expect(response.text).toBe('Hello World from Node with TypeScript!!!');
+        expect(response.text).toBe('Hello World from Node with TypeScript!');
     });
 });
